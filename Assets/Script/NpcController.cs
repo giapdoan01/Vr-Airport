ca210799ef864f1ad7eb50e71e4eb6c5ca44fd15
giapdoan01 <126212968+giapdoan01@users.noreply.github.com>
2025-11-26 17:28:09 +0700

    Add metadata police and security

--- Assets/Script/NpcController.cs
+++ Assets/Script/NpcController.cs
@@ -7,24 +7,6 @@ public class NpcController : MonoBehaviour
 {
     public AIDestinationSetter Destination;
     public Animator ani;
-    public GameObject player;
-    public float activeDistance = 10f; // Khoảng cách để hiện nút
-    public GameObject buttonWarn;
-
-    private void Start()
-    {
-        if (player == null)
-        {
-            player = GameObject.FindGameObjectWithTag("Player");
-        }
-
-       
-    }
-
-    private void Update()
-    {
-       
-    }
 
     public void WarnNpc()
     {
